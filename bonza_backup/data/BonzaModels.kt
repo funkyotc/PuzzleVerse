063d784ca5caaffbf5de0ef2dba6e43a680c5394
funkyotc <funkyotc@gmail.com>
2026-01-19 21:48:00 +1100

    code cleanup

--- bonza_backup/data/BonzaModels.kt
+++
@@ -1,4 +0,0 @@
-package com.funkyotc.puzzleverse.bonza.data
-
-// This file is intentionally left blank to resolve a redeclaration issue.
-// The data classes have been moved to their own files.
--- bonza_backup/data/BonzaPuzzle.kt
+++
@@ -1,6 +0,0 @@
-package com.funkyotc.puzzleverse.bonza.data
-
-data class BonzaPuzzle(
-    val clue: String,
-    val words: List<BonzaWord>
-)
