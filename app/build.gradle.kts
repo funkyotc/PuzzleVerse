035566c2b3b8fa34fb6bf85d561b4c236926ed7c
funkyotc <funkyotc@gmail.com>
2026-07-02 17:27:17 +1000

    chore: set dynamic versionCode using github run number to resolve Obtainium conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,15 @@ android {
         version = release(36)
     }
 
-    defaultConfig {
-        applicationId = "com.funkyotc.puzzleverse"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+defaultConfig {
+    applicationId = "com.funkyotc.puzzleverse"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = project.findProperty('versionCode')?.toInteger() ?: 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
 
     buildTypes {
         release {
